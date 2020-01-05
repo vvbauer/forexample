374fddc929fb797fde83fbf95ecebdb4249001fa
Vadim Bauer <polosatyrt@gmail.com>
2020-01-05 16:04:00 +0300

    control flow: (1) sequence (2) loop (3) conditional

--- 001_hello_world/main.go
+++ 001_hello_world/main.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
+
 	foo()
+
 	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
 
 func foo() {
