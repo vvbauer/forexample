1e271e706c5baaff2a35a06a5a5dd7175632152f
Vadim Bauer <polosatyrt@gmail.com>
2020-01-28 12:14:37 +0300

    Functions learning (upd return and multiple return)

--- functions/fnc.go
+++ functions/fnc.go
@@ -5,12 +5,30 @@ import "fmt"
 func main() {
 	foo()
 	bar("James")
+	s1 := woo("Moneypenny")
+	fmt.Println(s1)
+	x, y := mouse("Ian", "Flemming")
+	fmt.Println(x, y)
 }
 
+//basic func
 func foo() {
 	fmt.Println("hello from foo")
 }
 
+//arguments
 func bar(s string) {
 	fmt.Println("Hello", s)
+}
+
+//return
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo, ", st)
+}
+
+//multiple return
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, " ", ln, `, says "Hello"`)
+	b := false
+	return a, b
 }
